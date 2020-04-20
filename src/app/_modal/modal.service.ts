4abda1ca478c6b3aed9f32c23bfcd88e6a147424
Gerson Umanzor Alberto <gbumanzor@outlook.com>
2020-04-19 22:28:49 -0600

    :tada: Modal customization

---
+++ src/app/_modal/modal.service.ts
@@ -0,0 +1,29 @@
+﻿import { Injectable } from '@angular/core';
+import {ModalComponent} from "./modal.component";
+
+@Injectable({ providedIn: 'root' })
+export class ModalService {
+  private modals: ModalComponent[] = [];
+
+  add(modal: ModalComponent) {
+    // add modal to array of active modals
+    this.modals.push(modal);
+  }
+
+  remove(id: string) {
+    // remove modal from array of active modals
+    this.modals = this.modals.filter((x) => x.id !== id);
+  }
+
+  open(id: string) {
+    // open modal specified by id
+    const modal = this.modals.find((x) => x.id === id);
+    modal.open();
+  }
+
+  close(id: string) {
+    // close modal specified by id
+    const modal = this.modals.find((x) => x.id === id);
+    modal.close();
+  }
+}
